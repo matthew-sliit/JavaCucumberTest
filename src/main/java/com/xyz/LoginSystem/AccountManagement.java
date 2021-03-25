145a875b8f8b8671810cd1a1cd51e955d43154cc
Matthew Roche <it19126098@my.sliit.lk>
2021-03-25 18:51:36 +0530

    Initial commit

---
+++ src/main/java/com/xyz/LoginSystem/AccountManagement.java
@@ -0,0 +1,54 @@
+package com.xyz.LoginSystem;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class AccountManagement {
+	private class Account{
+		public String type = null, pass = null;
+		public Account(String type, String password) {
+			this.type = type; this.pass = password;
+		}
+	}
+	private static Map<String, Account> accountData;
+	private static int instance=0;
+
+	public AccountManagement() {
+		if(instance==0) {
+			accountData = new HashMap<>();
+		}
+		instance++;
+	}
+	private String generateSHA256(String password) {
+		return org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);
+	}
+	public String getType(String username, String password) {
+		if(accountData.containsKey(username)) {
+			Account account = accountData.get(username);
+			if(account.pass.equals(generateSHA256(password))) {
+				return account.type;
+			}
+			return "INVALID-PASSWORD";
+		}
+		return "INVALID-USERNAME";
+	}
+	public String createAccount(String type, String username, String password) {
+		if(accountData.get(username)!=null) {
+			return "ACCOUNT-EXISTS";
+		}
+		if(type.equals("STUDENT") || type.equals("TEACHER")) {
+			accountData.put(username, new Account(type, generateSHA256(password)));
+			return "SUCCESS";
+		}
+		return "INVALID-TYPE";
+	}
+	public String deleteAccount(String username, String password) {
+		if(getType(username, password).startsWith("INVALID-")) {
+			return getType(username, password);
+		}
+		accountData.remove(username);
+		return "SUCCESS";
+	}
+}
---
+++ src/test/java/com/xyz/LoginSystem/AccountManagementTest.java
@@ -0,0 +1,39 @@
+package com.xyz.LoginSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import io.cucumber.junit.CucumberOptions;
+
+//@RunWith(Cucumber.class) //not required for junit 5
+/*@CucumberOptions(
+ features = "Feature"
+ ,glue={"stepDefinition"}
+ )*/
+class AccountManagementTest {
+	@Test
+	void testAccountCreation() {
+		String username = "JohnFeatherington", password = "+n02PKs_hecto";
+		AccountManagement accountManager = new AccountManagement();
+		//create new account using username and password
+		accountManager.createAccount("STUDENT", username, password);
+		//check is account exists
+		assertEquals("STUDENT",accountManager.getType(username, password));
+		//if account exists, another account with same username shouldn't be created
+		assertNotEquals("SUCCESS", accountManager.createAccount("STUDENT", username, password));
+		//account data is set to a static variable, hence it should be saved event during the next time the class is initiated
+		accountManager = new AccountManagement();
+		assertNotEquals("SUCCESS", accountManager.createAccount("STUDENT", username, password));
+		//ensure the delete was successful
+		assertEquals("SUCCESS",accountManager.deleteAccount(username, password));
+		
+	}
+	
+	@Test
+	void testAccountTypes() {
+		String username = "JohnFeatherington", password = "+n02PKs_hecto";
+		AccountManagement accountManager = new AccountManagement();
+		//only types student and teacher exists, other types should be invalid
+		assertEquals("INVALID-TYPE", accountManager.createAccount("LECTURER", username, password));
+	}
+	
+}
